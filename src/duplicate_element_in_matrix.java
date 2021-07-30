181c8116983614b1b0087d8b5605efe5be5a7839
dipu843302 <dipu843302@gmail.com>
2021-07-30 07:29:33 +0530

    add

--- src/duplicate_element_in_matrix.java
+++ src/duplicate_element_in_matrix.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-
 public class duplicate_element_in_matrix {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
@@ -8,21 +10,32 @@ public class duplicate_element_in_matrix {
         int n=scanner.nextInt();
         System.out.println("Enter the element of array");
         int arr[][]=new int[m][n];
-        for (int i=0;i<n;i++){
-            for (int j=0;j<n;j++){
+        for (int i=0;i<m;i++){
+             for (int j=0;j<n;j++){
                 arr[i][j]=scanner.nextInt();
             }
         }
-        int arr2[]=new int[m*n];
-        for (int i=0;i<n;i++){
+        ArrayList<Integer>arrayList=new ArrayList<>();
+        for (int i=0;i<m;i++){
             for (int j=0;j<n;j++){
-                arrarr[i][j]
+                arrayList.add(arr[i][j]);
             }
         }
-        for (int i=0;i<n;i++){
-            for (int j=0;j<n;j++){
-                if (arr[i])
+        HashMap<Integer,Integer>hashMap=new HashMap<>();
+        for (int i=0;i<arrayList.size();i++){
+            if (hashMap.containsKey(arrayList.get(i))){
+                int count=hashMap.get(arrayList.get(i));
+                hashMap.put(arrayList.get(i),count+1);
+            }else {
+                hashMap.put(arrayList.get(i),1);
             }
+        }
+        System.out.println("Duplicates element in the array ");
+        for (Map.Entry<Integer,Integer>map:hashMap.entrySet()){
+            if (map.getValue()>1){
+                System.out.println(map.getKey());
+            }
+        }
         }
     }
-}
+
